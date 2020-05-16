ff09b18b46ee8566233e3ea6d7c7332143cfb065
e.voronova <voronova.eal@gmail.com>
2020-05-16 17:22:30 +0700

    Подготовлена фикстура для работы с веб-приложением Mantis, а также реализованы логин, логаут и тест на проверку их работы

---
+++ fixture/application.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(2)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def destroy(self):
+        self.wd.quit()
+
+    def open_home_page_by_url(self):
+        wd = self.wd
+        if not (len(wd.find_elements_by_xpath("(//input[@value='Login'])")) > 0):
+            wd.get(self.base_url)
